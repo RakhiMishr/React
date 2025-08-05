9e5c1c2d27d61772b155ccd168f78e4b093766be
Rakhi mishra <rakhimishra9572@gmail.com>
2025-08-05 22:43:56 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -1,15 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
-import Navbar from './Navbar';  
-import Cards from './Cards';
 
-function App() {
+const App = () => {
   return (
     <>
-    <Navber/>
-    <Cards/>
+      <div>
+        <nav>
+          <ul>
+            <li><a href="/" classname="active">HOME</a></li>
+            <li><a href="/">ABOUT</a></li>
+            <li><a href="/">OUR ROOM</a></li>
+            <li><a href="/">GALLERY</a></li>
+            <li><a href="/">CONTACTUS</a></li>
+            <li><a href="/">BLOG</a></li>
+
+          </ul>
+        </nav>
+        <div id="container">
+          <div id="logo">
+            <h1>Login</h1>
+            <input type="name" placeholder="name" id="username"  />
+            <input type="email" placeholder="email" id="email"  />
+            <input type="number" placeholder="phone" id="phone"  />
+            <input type="password" placeholder="password" id="password" />
+            <button type="submit" id="login_btn">login</button>
+          </div>
+        </div>
+        <div id="footer">
+          <p>Copyright © 2025 KETO. All rights reserved.</p>
+        </div>
+
+      </div>
     </>
-    );
+  )
 }
 
-export default App;
+export default App
+// This code defines a simple React component that renders a navigation bar, a login form, and a footer.
